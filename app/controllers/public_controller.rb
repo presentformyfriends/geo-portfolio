2a2218181c16b58c448dc081a99c9d4fca7b4017
presentformyfriends <devmike942@gmail.com>
2021-12-27 05:39:38 -0500

    change public_controller.rb

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,7 +2,7 @@ class PublicController < ApplicationController
     def index
         @city = request.location.city
         @country = request.location.country_code
-        @currency = @country.upcase == "AU" ? "AUD" : "USD"
+        @currency = @country.upcase == "AU" ? "AUD$" : "USD$"
     end
 
 end
