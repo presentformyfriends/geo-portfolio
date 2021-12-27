0c97e792a77377d77c8ecec126f6a2118988c1af
presentformyfriends <devmike942@gmail.com>
2021-12-27 03:20:37 -0500

    add geocoder

---
+++ app/controllers/public_controller.rb
@@ -0,0 +1,8 @@
+class PublicController < ApplicationController
+    def index
+        @city = request.location.city
+        @country = request.location.country
+        @currency = request.currency
+    end
+
+end
